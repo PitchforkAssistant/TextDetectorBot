f8ba2225551ce176b362b92deef18ea9796c283c
PitchforkAssistant <pitchforkassistant@gmail.com>
2019-08-07 11:17:45 +0300

    remove old try/catch, other tweaks/changes

--- redditmonitor.py
+++ redditmonitor.py
@@ -42,7 +42,7 @@ class RedditMonitor:
     def inbox_monitor_loop(self):
         stream = praw.models.util.stream_generator(self.reddit.inbox.unread)
         for reply in stream:
-            self.config.logger.debug("Messaged by {0}!".format(reply.author))
+            self.config.logger.debug(f"Messaged by {reply.author}!")
             if not isinstance(reply, praw.models.Comment):
                 continue
             # Because reply.is_submitter is not avaiable from inbox.
@@ -50,42 +50,38 @@ class RedditMonitor:
                 continue
             if not reply.parent().stickied:
                 continue
-            self.config.logger.debug("Removal disputed, reporting!")
+            self.config.logger.info(f"{reply.submission.id} removal disputed!")
             report = str(self.config.human_review_report).format(
                 username=reply.author.name)
             reply.parent().report(report[:100])
             reply.mark_read()
             self.config.logger.debug("Reported.")
-        self.config.logger.debug("Inbox monitor loop has ended!")
+        self.config.logger.warn("Inbox monitor loop has ended!")
 
     def posts_monitor_loop(self):
-        try:
-            subreddit = self.reddit.subreddit("mod")
-            for post in subreddit.stream.submissions():
-                self.config.logger.debug("New post: " + post.permalink)
-                if post.is_self or post.approved:
-                    continue
-                image = imagedownloader.DownloadImage(post.url)
-                if image is None:
-                    self.config.logger.debug("Got no image from: " + post.url)
-                    continue
-                textdetector = TextDetector(image)
-                if not textdetector.has_text():
-                    self.config.logger.debug("Text not found in image.")
-                    continue
-                self.config.logger.debug(
-                    "Removing {0}, text found: {1}".format(
-                        post.permalink, textdetector.get_text()))
-                post.mod.remove()
-                reason = str(self.config.removal_reason).format(
-                    username=post.author.name,
-                    subreddit=post.subreddit.display_name,
-                    text=", ".join(textdetector.get_text()))
-                reply = post.reply(reason)
-                reply.mod.distinguish(sticky=True)
-                self.config.logger.debug(post.permalink + " has been removed!")
-        except:
-            # I'm catching all exceptions and logging them, most likely,
-            # something went wrong with processing/downloading the image.
-            self.config.logger.exception(
-                "Exception in posts_monitor_loop:")
+        subreddit = self.reddit.subreddit("mod")
+        for post in subreddit.stream.submissions():
+            self.config.logger.debug("New post: " + post.permalink)
+            if post.is_self or post.approved:
+                continue
+            image = imagedownloader.DownloadImage(post.url)
+            if image is None:
+                self.config.logger.debug(f"Got no image from: {post.url}")
+                continue
+            textdetector = TextDetector(image)
+            if not textdetector.has_text():
+                self.config.logger.debug("Text not found in image.")
+                continue
+            self.config.logger.info(
+                "Removing {0}, text found: {1}".format(
+                    post.permalink, textdetector.get_text()))
+            self.config.logger.debug(textdetector._image_data)
+            post.mod.remove()
+            reason = str(self.config.removal_reason).format(
+                username=post.author.name,
+                subreddit=post.subreddit.display_name,
+                text=", ".join(textdetector.get_text()))
+            reply = post.reply(reason)
+            reply.mod.distinguish(sticky=True)
+            self.config.logger.debug(post.permalink + " has been removed!")
+        self.config.logger.warn("Posts monitor loop has ended!")
